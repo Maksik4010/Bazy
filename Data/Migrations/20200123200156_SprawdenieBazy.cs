45f93a2518191f593e8dae4f1feb74b6865a904a
Maks Wasiluk <Maksik4010@gmail.com>
2020-01-23 21:06:42 +0100

    Baza działa

---
+++ Data/Migrations/20200123200156_SprawdenieBazy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication16.Data.Migrations
+{
+    public partial class SprawdenieBazy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
